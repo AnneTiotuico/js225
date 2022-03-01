1292c4e3c37520624c9b5c9202094c3a8643457c
EC2 Default User <amtiotuico@gmail.com>
2022-03-01 15:35:05 +0000

    makeCar object factory

---
+++ lesson2/makeCar.js
@@ -0,0 +1,29 @@
+// 1. Write a makeCar function that works as shown above.
+
+function makeCar(accelRate, brakeRate) {
+  return {
+    speed: 0,
+    accelRate,
+    brakeRate,
+    accelerate() {
+      this.speed += this.accelRate;
+    },
+    brake() {
+      this.speed -= this.brakeRate;
+      this.speed < 0 ? this.speed = 0 : this.speed;
+    }
+  };
+}
+
+// let hatchback = makeCar(9);
+
+let sedan = makeCar(8, 6);
+sedan.accelerate();
+console.log(sedan.speed);
+// 8
+sedan.brake();
+console.log(sedan.speed);
+// 2
+sedan.brake();
+console.log(sedan.speed);
+// 0
